87cae2a3ad4f12b10030646cf24d0b9b967438a3
J Sterling <jsterling9@my.bcit.ca>
2020-06-13 02:13:44 -0700

    css update

--- header.php
+++ header.php
@@ -21,9 +21,8 @@
 	<link rel="stylesheet" href="style.css">
 	<title>Jarrod Sterling | UX Designer & Web Developer | Vancouver, B.C.</title>
 	
-	<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+	<link rel="icon" href="images/favicon.ico">
+	<link rel="shortcut icon" href="images/favicon.ico">
 
 	<?php wp_head(); ?>
 </head>
